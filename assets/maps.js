85991e7888a00aac732a6970e2a6913637d6651a
Matt Boland <mattjamesboland@gmail.com>
2020-05-07 14:54:33 +0000

    initial commit

---
+++ assets/maps.js
@@ -0,0 +1,30 @@
+function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 3,
+                center: {
+                    lat: 46.1341,
+                    lng: -4.7021,
+                }
+            });
+
+            var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            var locations = [
+                {lat: 53.3352, lng: -6.2285},
+                {lat: 51.4559, lng: -0.3415},
+                {lat: 51.4782, lng: -3.1826},
+                {lat: 55.9422, lng: -3.2409},
+                {lat: 48.92442731, lng: 2.36011326},
+                {lat: 41.9341, lng: 12.4547},
+            ];
+
+            var markers = locations.map(function(location, i){
+                return new google.maps.Marker({
+                    position: location,
+                    label: labels[i % labels.length]
+                });
+            });
+
+            var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }

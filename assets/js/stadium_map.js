2886065200640c4aa3625e7ba82415bb82baf703
Matt Boland <mattjamesboland@gmail.com>
2020-05-07 21:25:40 +0000

    Separate maps

--- assets/js/stadium_map.js
+++ assets/js/stadium_map.js
@@ -1,5 +1,5 @@
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
+    var map = new google.maps.Map(document.getElementById("stadium-map"), {
         zoom: 3,
         center: {
             lat: 46.1341,

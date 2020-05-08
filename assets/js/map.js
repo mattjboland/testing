43ba5765911bf6597bbde86b88f5d3429e06c98f
Matt Boland <mattjamesboland@gmail.com>
2020-05-08 11:32:33 +0000

    Clean up code, with comments

--- assets/js/map.js
+++ assets/js/map.js
@@ -3,49 +3,52 @@ var hotelMap;
 var stadiumMap;
 var otherInfo = [];
 
+// All stadium information
 var stadiums = [
     {
         id: "aviva",
         name: "Aviva Stadium",
         location: {lat: 53.3352, lng: -6.2285},
-        info: '<div id="content"><b>Aviva Stadium</b><div id="bodyContent"><a id="avivaHotels" href="#" onclick="updateHotelsMap(aviva);return false;">Click here to see hotels nearby...</a></p></div>'
+        info: '<div id="content"><h4>Aviva Stadium</h4></div>'
     },
     {
         id: "twickenham",
         name: "Twickenham Stadium",
         location: {lat: 51.4559, lng: -0.3415},
-        info: '<div id="content"><h3>Twickenham Stadium</h3>'
+        info: '<div id="content"><h4>Twickenham Stadium</h4></div>'
     },
     {
         id: "millennium",
         name: "Millennium Stadium",
         location: {lat: 51.4782, lng: -3.1826},
-        info: '<div id="content"><h3>Millennium Stadium</h3></div>'
+        info: '<div id="content"><h4>Millennium Stadium</h4></div>'
     },
     {
         id: "murrayfield",
         name: "Murrayfield Stadium",
         location: {lat: 55.9422, lng: -3.2409},
-        info: '<div id="content"><h3>Murrayfield Stadium</h3>'
+        info: '<div id="content"><h4>Murrayfield Stadium</h4></div>'
     },
     {
         id: "stade",
         name: "Stade de France",
         location: {lat: 48.92442731, lng: 2.36011326},
-        info: '<div id="content"><h3>Stade de France</h3>'
+        info: '<div id="content"><h4>Stade de France</h4></div>'
     },
     {
         id: "stadio",
         name: "Stadio Olimpico",
         location: {lat: 41.9341, lng: 12.4547},
-        info: '<div id="content"><h3>Stadio Olimpico</h3>'
+        info: '<div id="content"><h4>Stadio Olimpico</h4></div>'
     }
 ];
 
+// Add all the stadium markers
 function addStadiums() {
 
     var stadiumMarkers = stadiums.map(function(stadium, i){
-
+        
+        // Add marker for each stadium
         var marker = new google.maps.Marker({
             map: stadiumMap,
             draggable: false,
@@ -55,11 +58,13 @@ function addStadiums() {
             icon: 'assets/images/rugby_ball.png'
         });
 
+        // Add info bubble
         var infowindow = new google.maps.InfoWindow({
             content: stadiums[i].info,
             maxWidth: 200
         });
 
+        // When user clicks on the marker, show info bubble and update hotel map
         marker.addListener('click', function () {
             closeOtherInfo();
             infowindow.open(stadiumMap, marker);
@@ -70,9 +75,11 @@ function addStadiums() {
         return marker;
     });
 
+    // Add the markers
     var stadiumMarkerCluster = new MarkerClusterer(stadiumMap, stadiumMarkers,{imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }
 
+// When clicking on a marker, close the open bubble 
 function closeOtherInfo() {
     if (otherInfo.length > 0) {
         otherInfo[0].set("marker", null);
@@ -81,38 +88,28 @@ function closeOtherInfo() {
     }
 }
 
+// Search for hotels around the stadium and update the hotels map
 function updateHotelsMap(stadium) {
     alert('Showing hotels for '+stadium);
 }
 
+// Main function from index.html to initialise the maps
 function initMap() {
     
     // Hotels Map
     hotelMap = new google.maps.Map(document.getElementById("hotels"), {
         zoom: 3,
+        streetViewControl: true,
         center: {
             lat: 46.1341,
             lng: -4.7021,
         }
     });
-
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    /*
-    var hotelMarkers = stadiumLocations.map(function(location, i){
-        return new google.maps.Marker({
-            draggable: false,
-            animation: google.maps.Animation.DROP,
-            position: location,
-            label: labels[i % labels.length],
-        });
-    });
-
-    var hotelMarkerCluster = new MarkerClusterer(hotelMap, hotelMarkers,{imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-    */
-
+    
+    // Stadiums Map
     stadiumMap = new google.maps.Map(document.getElementById("stadiums"), {
         zoom: 3,
+        streetViewControl: false,
         center: {
             lat: 46.1341,
             lng: -4.7021,

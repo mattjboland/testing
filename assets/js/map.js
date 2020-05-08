003e087d313292113b386588a953615936933463
Matt Boland <mattjamesboland@gmail.com>
2020-05-08 14:57:15 +0000

    Change zoom for hotel search

--- assets/js/map.js
+++ assets/js/map.js
@@ -90,7 +90,7 @@ function closeOtherInfo() {
 // Search for hotels around the stadium and move map to that location
 function updateHotelsMap(stadiumIndex) {
     hotelMap.panTo(stadiums[stadiumIndex].location);
-    hotelMap.setZoom(20);
+    hotelMap.setZoom(10);
     
     // Search for hotels in the selected city, within the viewport of the map.
     var search = {
